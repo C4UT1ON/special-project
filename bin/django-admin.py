114289864cd8de211f5a8eef5f2a862b25fc5384
LeRon Harris <lharris@leaplab.dev>
2019-07-08 20:50:54 -0500

    init setup

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rico/Documents/Projects/reservation_system/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ resources/urls.py
@@ -0,0 +1,9 @@
+from resources import views
+from django.urls import path
+
+urlpatterns = [
+    path('items', views.ItemList.as_view()),
+    path('items/<int:pk>', views.ItemObject.as_view()),
+    path('bookings', views.BookingList.as_view()),
+    path('bookings/<int:pk>', views.BookingObject.as_view())
+]
