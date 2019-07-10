319ff1949c3a9e31056fcc9423a3ce2928b1db93
Rico Suave <ricardo.m.coleman>
2019-07-10 15:44:32 +0000

    python anywhere integration

--- reservation_system/urls.py
+++ reservation_system/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from reservation_system import views
 from django.urls import path
 
@@ -6,27 +5,5 @@ urlpatterns = [
     path('items', views.ItemList.as_view()),
     path('items/<int:pk>', views.ItemObject.as_view()),
     path('bookings', views.BookingList.as_view()),
-    path('bookings/<int:pk>', views.BookingObject.as_view())
-=======
-"""reservation_system URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('res/', include('resources.urls'))
->>>>>>> 9142a665114861ff7c76413fa1846192ce1e6da6
-]
+    path('bookings/<int:pk>', views.BookingObject.as_view()),
+    ]
--- resources/urls.py
+++
@@ -1,9 +0,0 @@
-from resources import views
-from django.urls import path
-
-urlpatterns = [
-    path('items', views.ItemList.as_view()),
-    path('items/<int:pk>', views.ItemObject.as_view()),
-    path('bookings', views.BookingList.as_view()),
-    path('bookings/<int:pk>', views.BookingObject.as_view())
-]
